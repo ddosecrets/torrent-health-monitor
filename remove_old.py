f6cfd354a136da62d8363aee20d3be629dfd9512
Milo Trujillo <milo.trujillo@ddosecrets.com>
2025-12-31 13:09:03 -0700

    Now removes old records from tracker_availability

--- remove_old.py
+++ remove_old.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 from database import database
-from datetime import datetime
+import time
 
 """
-	Prunes database entries over 30 days old, as these records are never
-	shown in UI and needlessly log addresses
+	This script removes log entries more than 32 days old. We only display
+	entries up to a month old in the UI, so these old records will never be
+	used and will slowly fill the disk.
 """
 
 if __name__ == "__main__":
-	one_month = 2592000
-	earliest_record = int(datetime.now().strftime("%s")) - one_month
+	threshold = 2764800 # 32 days in seconds
+	earliest_record = int(time.time()) - threshold
 	with database() as (conn,c):
 		c.execute("DELETE FROM peers WHERE epoch < %s", [earliest_record])
+		c.execute("DELETE FROM tracker_availability WHERE epoch < %s", [earliest_record])
